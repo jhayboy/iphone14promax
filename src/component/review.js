336ea57c9fc9ec1510c9fdd56b733be476d1b8cd
Jhayboy <mbamarajustice1@gmail.com>
2023-11-12 09:23:34 +0100

    new review

--- src/component/review.js
+++ src/component/review.js
@@ -30,6 +30,95 @@ import {AiFillStar} from "react-icons/ai"
 const StaticReview = () => {
   return (
     <div className="bg-black p-4 rounded-lg shadow-md mb-4 flex flex-col gap-3">
+      
+
+      <div>
+            <div className="flex justify-between items-center ">
+               
+                <div className="text-white flex items-center gap-2">
+                    <div className="rounded-full bg-green-400 p-2 w-10 h-10 text-center">
+                        <p>Q</p>
+                    </div>
+                    <div>
+                        <h3 className="text-sm font-semibold ">Queen Lizzy</h3>
+                        <p className="text-xs">11-10-23</p>
+                    </div>
+                </div>
+                <div className="flex items-center">
+                <span className="text-yellow-500 mr-1 flex gap-1">
+                    <AiFillStar/>
+                    <AiFillStar/>
+                    <AiFillStar/>
+                    <AiFillStar/>
+                    <AiFillStar className="text-gray-500"/>
+                </span>
+                <span className="text-gray-500">(4.0)</span>
+                </div>
+            </div>
+            <p className="mt-2 text-gray-500 text-sm">
+                I've never been this excited about a purchase. The iPhone 14 Pro Max for $499 is a dream come true
+            </p>
+        </div>
+
+
+       <div>
+            <div className="flex justify-between items-center ">
+               
+                <div className="text-white flex items-center gap-2">
+                    <div className="rounded-full bg-green-400 p-2 w-10 h-10 text-center">
+                        <p>L</p>
+                    </div>
+                    <div>
+                        <h3 className="text-sm font-semibold ">Lisa</h3>
+                        <p className="text-xs">11-09-23</p>
+                    </div>
+                </div>
+                <div className="flex items-center">
+                <span className="text-yellow-500 mr-1 flex gap-1">
+                    <AiFillStar/>
+                    <AiFillStar/>
+                    <AiFillStar/>
+                    <AiFillStar className="text-gray-500"/>
+                    <AiFillStar className="text-gray-500"/>
+                </span>
+                <span className="text-gray-500">(3.0)</span>
+                </div>
+            </div>
+            <p className="mt-2 text-gray-500 text-sm">
+                Why cant i purchase as much as i want, this is not fair
+            </p>
+        </div>
+
+
+      <div>
+            <div className="flex justify-between items-center ">
+               
+                <div className="text-white flex items-center gap-2">
+                    <div className="rounded-full bg-green-400 p-2 w-10 h-10 text-center">
+                        <p>D</p>
+                    </div>
+                    <div>
+                        <h3 className="text-sm font-semibold ">Daniel Scott</h3>
+                        <p className="text-xs">11-09-23</p>
+                    </div>
+                </div>
+                <div className="flex items-center">
+                <span className="text-yellow-500 mr-1 flex gap-1">
+                    <AiFillStar/>
+                    <AiFillStar/>
+                    <AiFillStar/>
+                    <AiFillStar/>
+                    <AiFillStar/>
+                </span>
+                <span className="text-gray-500">(5.0)</span>
+                </div>
+            </div>
+            <p className="mt-2 text-gray-500 text-sm">
+                I was skeptical at first, but this website delivered on its promise. A phone worth $1000 for $499? Count me in! 😍
+            </p>
+        </div>
+
+
       <div>
         <div className="flex justify-between items-center ">
             <div className="text-white flex items-center gap-2">
@@ -172,6 +261,7 @@ const StaticReview = () => {
                 I was really surprised when it was delivered, was thinking it was refurbished but it wasn't 100% legit. little secret use a different device to purchase as many as possible
             </p>
         </div>
+
     </div>
   );
 };
